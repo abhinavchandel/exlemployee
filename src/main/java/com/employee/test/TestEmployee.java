2f70d1c68807defac8527047575a3fb9afd428c4
Abhinav Singh Chandel <2020mt93030@wilp.bits-pilani.ac.in>
2021-10-19 10:28:20 +0530

    Removed test case and comment in property file

--- src/main/java/com/employee/test/TestEmployee.java
+++ src/main/java/com/employee/test/TestEmployee.java
@@ -1,38 +1,21 @@
 package com.employee.test;
 
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Test;
- 
-
 public class TestEmployee {
-	
-	@Test
-	public void givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived()
-	  throws ClientProtocolException, IOException {
-	 
-	    // Given
-	     
-	    HttpUriRequest request = new HttpGet( "http://localhost:8085");
-
-	    // When
-	    HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
-
-	    // Then
-	    assertThat(
-	      httpResponse.getStatusLine().getStatusCode(),
-	      equalTo(HttpStatus.SC_NOT_FOUND));
-	}
+	/*
+	 * @Test public void
+	 * givenUserDoesNotExists_whenUserInfoIsRetrieved_then404IsReceived() throws
+	 * ClientProtocolException, IOException {
+	 * 
+	 * // Given
+	 * 
+	 * HttpUriRequest request = new HttpGet( "http://localhost:8085");
+	 * 
+	 * // When HttpResponse httpResponse =
+	 * HttpClientBuilder.create().build().execute( request );
+	 * 
+	 * // Then assertThat( httpResponse.getStatusLine().getStatusCode(),
+	 * equalTo(HttpStatus.SC_NOT_FOUND)); }
+	 */
 	
 	
 }
